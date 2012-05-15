9c37e99597b42296bb4d0c7806bbdc36e8212304
Deepak Prasanna <deepak@systeminsights.com>
2012-05-15 15:38:49 +0530

    replset connection from scala

--- src/main/scala/reader.scala
+++ src/main/scala/reader.scala
@@ -1,14 +1,17 @@
+import com.mongodb._ 
 import com.mongodb.casbah.Imports._ 
 
 object MongoAnalysisDB {
   def main(args: Array[String]) = {
     //that connection
-    val mongoConn = MongoConnection("127.0.0.1", 27018) 
-    val mongoDB = mongoConn("vimana-analysis") 
-    val mongoColl = mongoConn("vimana-analysis")("mtcevents") 
+    val addresses = List(new ServerAddress("127.0.0.1" , 27018),  new ServerAddress("127.0.0.1" , 27019),  new ServerAddress( "127.0.0.1" , 27020 ))
+    val mongoConn = MongoConnection(addresses) 
+    val mongoDB = mongoConn("vimana-sandbox-dup") 
+    val mongoColl = mongoConn("vimana-sandbox-dup")("utilization.metrics.cycledowntime") 
+
 
     //that query
-    val loadEvent = MongoDBObject("jsonClass" -> "Load") 
+    val loadEvent = MongoDBObject("period" -> "PT1H") 
     val cursor = mongoColl.find(loadEvent)
     val mtcevent = mongoColl.findOne(loadEvent)
 
